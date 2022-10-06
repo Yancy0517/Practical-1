61c504e1be16e92c008d50d2d84b775fec94df3e
nieheyu <nieheyuuk@outlook.com>
2022-10-06 20:50:02 +0800

    Heyu Nie Step 3-5

--- proj1.R
+++ proj1.R
@@ -10,3 +10,25 @@
 # step 10 was done by Linsheng Shu alone 
 # The rest of the steps were written individually. At the end, the group carried out a comparison and discussion of each person's code to find the best approach.
 
+#3
+setwd("C:\\Users\\niehe\\Desktop\\S.P")
+a <- scan("pg10.txt",what="character",skip=104) ## skip contents
+n <- length(a)
+a <- a[-((n-2886):n)]
+a <- a[-grep("[0123456789]:[0123456789]", a)] ## Strip out verse numbers
+
+#4
+# Split_punct function can separate the punctuation marks from words
+split_punct <- function(a, punct){
+  bible <- a 
+  bible <- gsub("[()]", "", bible) # Remove "(" and ")" .
+  ii <- grep(punct, bible) # Position of words with a punctuation mark
+  split_p<- rep ("", length(ii)+ length(bible)) # New vector to store words and punctuation mark
+  iis <- ii+1:length(ii) # new position of punctuation mark.
+  split_p[iis]<- substr(bible[ii],nchar(bible[ii]),nchar(bible[ii])) # Add punctuation mark to new vetor
+  bible<- gsub(punct,"" , bible) # Remove punctuation marks from words
+  split_p[-iis] <- substr(bible,0,nchar(bible[1:length(bible)])) # Add words to new vector
+  return(split_p) # Return final vector.
+}
+#5 
+after_split <- split_punct(a, "[,.?!:;]") # Separate the punctuation marks
