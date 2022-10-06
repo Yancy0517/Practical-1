c3d720882d0f70ba4ce30076a479b50200fa8436
linshengs <linsheng.shu@outlook.com>
2022-10-06 13:38:55 +0100

    comment part

---
+++ proj1.R
@@ -0,0 +1,11 @@
+#1
+# Team member information:
+# s2404675 Heyu Nie
+# s2281875 Jialong He
+# s2317223 Linsheng Shu
+
+#2
+# Step 4 was done by  Heyu Nie alone 
+# Step 7 was done by Jialong He alone 
+# step 10 was done by Linsheng Shu alone 
+# The rest of the steps were written individually. At the end, the group carried out a comparison and discussion of each person's code to find the best approach.
