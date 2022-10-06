f51538cd870d85532336911a96a1086891a5a616
linshengs <linsheng.shu@outlook.com>
2022-10-06 13:58:07 +0100

    Step 10 and step 8-9 by Linsheng Shu

--- proj1.R
+++ proj1.R
@@ -32,6 +32,7 @@ split_punct <- function(a, punct){
 }
 #5 
 after_split <- split_punct(a, "[,.?!:;]") # Separate the punctuation marks
+
 #6
 lowerb <- tolower(after_split) # Lowercase the upper case text in the data
 unique_word <- unique(lowerb) # Find the unique words in the "lowerb"
@@ -39,6 +40,22 @@ indicies <- match(lowerb,unique_word) # Position of each unique word
 amount <- tabulate(indicies) # Counting the number of each unique word.
 lowerp <- order(amount,decreasing = TRUE) # Sort the occurrences of each unique word in "amount" from most to least (The output is the location)
 b <- unique_word[lowerp[1:500]] # The 500 most frequently occurring words
+
+#10 We insert the code needed in step 10 here
+sortb <- sort(amount, decreasing = TRUE) # Sort the occurrences of each unique word in "amount" from most to least (The output is the number of occurrences)
+number_of_occurrences <- sortb[1:500] # Number of occurrences corresponding to the 500 most frequently occurring words
+bm <- cbind(b,number_of_occurrences) # Create a matrix of "b" and their corresponding numbers
+
+capitalp <- grep("[A-Z]", after_split) #Find the positions of the capitalized word
+capital <- after_split[capitalp] # Capital words
+unique_capital <- unique(capital) # unique capitalized words
+lower_unique_capital <- tolower(unique_capital) # Lowercase all words in "unique_capital"
+position_in_b <- match(lower_unique_capital, b) # match "lower_unique_capital" and "b"
+de <- which(is.na(position_in_b)) #Position of all output NA in "position_in_b"
+unique_capital<- unique_capital[-de] # Find words in the Bible that appear with capital letters and are also in "b".
+position_in_as <- match(after_split, unique_capital) # Match after_split and unique_capital
+amount_of_capital <- tabulate(position_in_as) # Counting the number 
+cm <- cbind(unique_capital,amount_of_capital) # Create a matrix of uppercase words and their corresponding numbers
 
 #7
 # probability of Three-dimensional arrays: T 
@@ -94,3 +111,34 @@ for(i in 1:500){
     }
   }
 } 
+
+#8
+print_txt_position <- c() # build a set
+# Create a for loop and use S,A,T in the loop to simulate the random generation of 50 words
+for(i in 1:50){
+  if(i==1){                                                       # First word could only use S
+    print_txt_position[i] <-sample(1:500, 1,rep = TRUE, prob = S)
+  }
+  if(i==2){                                                       # second word could only use A or S
+    if(sum(A[print_txt_position[i-1],])!=0){
+      print_txt_position[i] <- sample(1:500, 1, replace = TRUE, prob = A[print_txt_position[i-1],]) 
+    }
+    else{
+      print_txt_position[i] <- sample(1:500, 1, replace = TRUE, prob = S)
+    }
+  }
+  if(i>2){                                                        # From now on consider the three cases S,A,T separately
+    if(sum(T[print_txt_position[i-2], print_txt_position[i-1],])!=0){
+      print_txt_position[i] <- sample(1:500, 1, replace = TRUE, prob = T[print_txt_position[i-2],print_txt_position[i-1],])
+    }else if(sum(A[print_txt_position[i-1],])!=0){
+      print_txt_position[i] <- sample(1:500, 1, replace = TRUE, prob = A[print_txt_position[i-1],]) 
+    }else{
+      print_txt_position[i] <- sample(1:500, 1, replace = TRUE, prob = S)
+    }
+  }
+}
+cat(b[print_txt_position]) 
+
+#9 
+# Simulate 50 word sections of text where the word probabilities are simply taken from S.
+cat(b[sample(1:500, 50, replace= TRUE, prob=S)]) 
